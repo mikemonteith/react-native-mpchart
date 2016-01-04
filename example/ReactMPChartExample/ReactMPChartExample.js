dfb727bccfb98c6e059fb4be1df16fc8599944bf
Mike Monteith <mike@mikemonteith.com>
2016-01-04 13:16:30 +0000

    Initial commit

---
+++ example/ReactMPChartExample/ReactMPChartExample.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var Chart = require('react-native-mpchart');
+
+module.exports = React.createClass({
+  render: function(){
+    return (
+      <Text>Example app</Text>
+    );
+  },
+});
---
+++ example/ReactMPChartExample/index.android.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+} = React;
+
+var ReactMPChartExample = require('./ReactMPChartExample.js');
+
+AppRegistry.registerComponent('ReactMPChartExample', () => ReactMPChartExample);
