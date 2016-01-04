af9844c0ae1034b4df1c733d30a9e00d16c5f5e9
Mike Monteith <mike@mikemonteith.com>
2016-01-04 17:06:08 +0000

    add select and deselect pie chart events

--- android/src/main/java/com/mikemonteith/reactnativempchart/MPChartPieManager.java
+++ android/src/main/java/com/mikemonteith/reactnativempchart/MPChartPieManager.java
@@ -2,19 +2,27 @@ package com.mikemonteith.reactnativempchart;
 
 import android.graphics.Color;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 
 public class MPChartPieManager extends SimpleViewManager<PieChart> {
     public static final String REACT_CLASS = "MPChartPie";
+
+    private ThemedReactContext context;
 
     @Override
     public String getName(){
@@ -22,8 +30,38 @@ public class MPChartPieManager extends SimpleViewManager<PieChart> {
     }
 
     @Override
-    protected PieChart createViewInstance(ThemedReactContext context) {
-        return new PieChart(context);
+    protected PieChart createViewInstance(final ThemedReactContext context) {
+        this.context = context;
+        final PieChart chart = new PieChart(context);
+
+        chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener(){
+
+            public void onValueSelected(Entry entry, int dataSetIndex, Highlight highlight){
+                WritableMap event = Arguments.createMap();
+                event.putString("type", "segmentSelect");
+                event.putDouble("value", (double) entry.getVal());
+                event.putInt("xIndex", entry.getXIndex());
+
+                context.getJSModule(RCTEventEmitter.class).receiveEvent(
+                        chart.getId(),
+                        "topSelect",
+                        event
+                );
+            }
+
+            public void onNothingSelected(){
+                WritableMap event = Arguments.createMap();
+                event.putString("type", "segmentDeselect");
+                context.getJSModule(RCTEventEmitter.class).receiveEvent(
+                        chart.getId(),
+                        "topSelect",
+                        event
+                );
+            }
+
+        });
+
+        return chart;
     }
 
     @ReactProp(name = "values")
--- src/PieChart.js
+++ src/PieChart.js
@@ -6,6 +6,16 @@ module.exports = React.createClass({
   propTypes: {
     colors: React.PropTypes.array,
     values: React.PropTypes.array,
+    onSegmentSelect: React.PropTypes.func,
+    onSegmentDeselect: React.PropTypes.func,
+  },
+
+  _onSelect: function(event){
+    if(event.type === "segmentSelect" && this.props.onSegmentSelect){
+      this.props.onSegmentSelect(event);
+    }else if(event.type === "segmentDeselect" && this.props.onSegmentDeselect){
+      this.props.onSegmentDeselect(event);
+    }
   },
 
   render: function(){
@@ -14,6 +24,7 @@ module.exports = React.createClass({
         style={[{flex: 1}, this.props.style]}
         colors={this.props.colors}
         values={this.props.values}
+        onSelect={this._onSelect}
       />
     );
   },
