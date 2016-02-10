252db7c95ce3347467fd6090fea3e42623b99916
Mike Monteith <mike@mikemonteith.com>
2016-02-10 20:59:34 +0000

    update to react-native 0.19.0

--- android/src/main/java/com/mikemonteith/reactnativempchart/MPChartPieManager.java
+++ android/src/main/java/com/mikemonteith/reactnativempchart/MPChartPieManager.java
@@ -2,7 +2,7 @@ package com.mikemonteith.reactnativempchart;
 
 import android.graphics.Color;
 
-import com.facebook.react.uimanager.ReactProp;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.Entry;
